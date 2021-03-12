a47da2e59929cc00df6f3ef9fe8ead9e5bae316d
Ali <alexander.antoshuk@gmail.com>
2021-03-12 15:27:24 +0300

    Add corner case test with Infinity

--- module16_homework/task1/sum.test.js
+++ module16_homework/task1/sum.test.js
@@ -3,3 +3,8 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+
+test('adds Infinity + 5 to equal Infinity', () => {
+    expect(sum(Infinity, 5)).toBe(Infinity);
+});
